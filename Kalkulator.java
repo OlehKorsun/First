ecff90968236c11715fdaaaf78e5818137272729
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-10-19 22:05:14 +0200

    Add files via upload

---
+++ Kalkulator.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class Kalkulator {
+	public static void main(String[] args){
+		String a = JOptionPane.showInputDialog(null, "Podaj pierwsza liczbe");
+		double x = Double.parseDouble(a);
+		
+		String b = JOptionPane.showInputDialog(null, "Podaj druga liczbe");
+		double y = Double.parseDouble(b);
+		
+		String c = JOptionPane.showInputDialog(null, "symbol dzialania");
+		
+		double wynik = 0;
+		
+		String plus = "+";
+		String minus = "-";
+		String iloczyn = "*";
+		String iloraz = "/";
+		
+		if(c.equals(plus) == true) {
+			wynik = x + y;
+		} 
+		if(c.equals(minus) == true) {
+			wynik = x - y;
+		}
+		if(c.equals(iloczyn) == true) {
+			wynik = x * y;
+		} 
+		if(c.equals(iloraz) == true) {
+			wynik = x / y;
+		} 
+		JOptionPane.showMessageDialog(null, wynik);
+	}
+}
---
+++ Trojkat.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class Trojkat {
+	public static void main(String[] args){
+		String x = JOptionPane.showInputDialog(null, "Wprowadz liczbe a");
+		double a = Double.parseDouble(x);
+		
+		String y = JOptionPane.showInputDialog(null, "Wprowadz liczbe b");
+		double b = Double.parseDouble(y);
+		
+		String z = JOptionPane.showInputDialog(null, "Wprowadz liczbe c");
+		double c = Double.parseDouble(z);
+		
+		double max = a;
+		double sum = b+c;
+		if(max<b){
+			max = b;
+			sum = a+c;
+		} else if(max<c) {
+			max = c;
+			sum = a+b;
+		}
+		if(sum>max){
+			JOptionPane.showMessageDialog(null, "Jest OK");
+		} else {
+			JOptionPane.showMessageDialog(null, "Nie jest OK");
+		}
+	}
+}
