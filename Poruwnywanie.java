ce1aad63f8a19e5356cbb057df59d10b9cc50273
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-10-28 22:55:12 +0200

    Update Poruwnywanie.java

--- Poruwnywanie.java
+++ Poruwnywanie.java
@@ -1,16 +1,12 @@
 import javax.swing.*;
-
 public class Poruwnywanie {
 	public static void main(String[] args){
 		String x = JOptionPane.showInputDialog(null, "Enter a");
 		int a = Integer.parseInt(x);
-		
 		String y = JOptionPane.showInputDialog(null, "Enter b");
 		int b = Integer.parseInt(y);
-		
 		String z = JOptionPane.showInputDialog(null, "Enter c");
 		int c = Integer.parseInt(z);
-		
 		if(a==b && a==c && b==c) {
 			JOptionPane.showMessageDialog(null, "Nie OK");
 		} else if(a==b && a!=c && b!=c) {
