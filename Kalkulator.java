3752ba826ae38ddcc04a3b8e0fec8222465fb237
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-10-28 22:57:53 +0200

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -3,19 +3,14 @@ public class Kalkulator {
 	public static void main(String[] args){
 		String a = JOptionPane.showInputDialog(null, "Podaj pierwsza liczbe");
 		double x = Double.parseDouble(a);
-		
 		String b = JOptionPane.showInputDialog(null, "Podaj druga liczbe");
 		double y = Double.parseDouble(b);
-		
 		String c = JOptionPane.showInputDialog(null, "symbol dzialania");
-		
 		double wynik = 0;
-		
 		String plus = "+";
 		String minus = "-";
 		String iloczyn = "*";
 		String iloraz = "/";
-		
 		if(c.equals(plus) == true) {
 			wynik = x + y;
 		} else if(c.equals(minus) == true) {
