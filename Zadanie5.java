fab50628019252f935632ab4a5d1daf101d54883
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-10-29 19:58:22 +0100

    Update Zadanie5.java

--- Zadanie5.java
+++ Zadanie5.java
@@ -6,7 +6,7 @@ public class Zadanie5 {
 		System.out.println("Wprowadz znak");
 		double a = scan.nextDouble();
 		double b = scan.nextDouble();
-		
+		// Суть задания: Проверить являются ли введённые два числа одинаковыми с точностью до двух знаков после запятой
 		if(a-b<0.01 && a-b>-0.01) {
 			System.out.println("Tak");
 		} else System.out.println("Nie");
