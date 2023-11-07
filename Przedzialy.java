ce5b5971e74d6ef0e077df897a08eaac4ad5a381
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-11-07 23:29:33 +0100

    Add files via upload

---
+++ Przedzialy.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Przedzialy {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Wprowadz a: ");
+        int a = scan.nextInt();
+
+        System.out.print("Wprowadz b: ");
+        int b = scan.nextInt();
+
+        System.out.print("Wprowadz c: ");
+        int c = scan.nextInt();
+
+        System.out.print("Wprowadz d: ");
+        int d = scan.nextInt();
+
+        boolean inSum, inIntersect, inSumDiff;
+
+        for(int n = -5; n<6; n++){
+            inSum = n>=a && n<=b || n>=c && n<=d;
+            inIntersect = n>=a && n<=b && n>=c && n<=d;
+            inSumDiff = n>=a && n<=b && n<=c || n>=c && n<=d && n>=b;
+
+            System.out.println(n + ": InSum? " + inSum + "; inIntersect? " + inIntersect + " ; inSymDiff? " + inSumDiff);
+        }
+    }
+}
