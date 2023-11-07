c4a3b82282f89158f0d90f99c811bd46f6b2a93e
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-11-07 23:30:04 +0100

    Add files via upload

---
+++ Karty.java
@@ -0,0 +1,32 @@
+public class Karty {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++) {
+            int kolor = (int) (Math.random()*4)+1;
+            int numer = (int) (Math.random()*13)+2;
+
+            String kol = switch (kolor){
+                case 1 -> "Trefl";
+                case 2 -> "Karo";
+                case 3 -> "Kier";
+                default -> "Pik";
+
+            };
+            String rank = switch(numer) {
+                case 2 -> "Dwojka";
+                case 3 -> "Trojka";
+                case 4 -> "Czworka";
+                case 5 -> "Piatka";
+                case 6 -> "Szustka";
+                case 7 -> "Siudemka";
+                case 8 -> "Usemka";
+                case 9 -> "Dziewiatka";
+                case 10 -> "Dziesiatka";
+                case 11 -> "Walet";
+                case 12 -> "Dama";
+                case 13 -> "Krol";
+                default -> "As";
+            };
+            System.out.println(rank + " " + kol);
+        }
+    }
+}
