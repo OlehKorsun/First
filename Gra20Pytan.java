1216ea91549ed646a759e842036bf642a6b9750b
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-11-07 23:30:19 +0100

    Add files via upload

---
+++ Gra20Pytan.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Gra20Pytan {
+     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = 1, b = 1000000;
+        int n = 500000;
+
+//        System.out.println("Czy jest to n? " + n);
+//        String odpowiedz = scan.next();
+
+//            while (!(odpowiedz.equals("y"))) {
+//                if (odpowiedz.equals("s")) {
+//                    a = n;
+//                    n = (a+b)/2;
+//
+//                } else if (odpowiedz.equals("b")) {
+//                    b = n;
+//                    n = (a+b)/2;
+//                }
+//                System.out.println("Czy jest to n? " + n);
+//                odpowiedz = scan.next();
+//            }
+         for(int i = 1; i<20; i++) {
+             System.out.println("Czy jest to n? " + n);
+             String odpowiedz = scan.next();
+             if (odpowiedz.equals("y")) {
+                 System.out.println("Pomyslna liczba to: " + n);
+                 return;
+             } else if (odpowiedz.equals("b")) {
+                 b = n;
+                 n = (a+b)/2;
+             } else if (odpowiedz.equals("s")) {
+                 a = n;
+                 n = (a+b)/2;
+             } else {
+                 System.out.println("Nieprawidlowa odpowiedz");
+             }
+         }
+         System.out.println("Nie udalo mi sie :_(");
+    }
+}
