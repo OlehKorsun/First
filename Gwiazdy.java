1a033a1bd7e7cfd14f6a267784d76ea69590c153
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-11-10 22:11:21 +0100

    Update Gwiazdy.java

--- Gwiazdy.java
+++ Gwiazdy.java
@@ -29,43 +29,5 @@ public class Gwiazdy {
                 System.out.print("*");
             }
         }
-
-
-
-//        for(int i = 0; i<=n && n>0; i++){
-//            if(i==n) {
-//                System.out.println();
-//                n-=1;
-//                for (; n<i; i-=2) {
-//                    System.out.print(" ");
-//                }
-//                n-=1;
-//                i=0;
-//            }
-//            System.out.print("*");
-//        }
-
-
-
-
-//        for(int i = 0; i<=n && n>0; i++){
-//            for(int j = i; n<=j; j--){
-//                System.out.println();
-//                System.out.print(" ");
-//            }
-//            System.out.print("*");
-//        }
-
-
-
-//        for(int i = 0; i<=n; i++) {
-//            for(int j = i ; j>0 ; j--) {
-//                System.out.print(" ");
-//
-//            }
-//            System.out.print("*");
-//        }
-
-
     }
 }
