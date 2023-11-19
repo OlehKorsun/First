010f481bcf14ed8fe02be56be8c957dca8e3b4a5
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-11-19 23:40:26 +0100

    Update OdwracanieKolejnosciLiczb.java

--- OdwracanieKolejnosciLiczb.java
+++ OdwracanieKolejnosciLiczb.java
@@ -10,8 +10,6 @@ public class OdwracanieKolejnosciLiczb {
             arr[j] = a;
         }
 
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ; ");
-        }
+        System.out.println(Arrays.toString(arr));
     }
 }
