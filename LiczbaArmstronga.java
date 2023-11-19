c0ecca64b621651b28894f0054c5d6af307a61c6
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-11-19 23:39:19 +0100

    Update LiczbaArmstronga.java

--- LiczbaArmstronga.java
+++ LiczbaArmstronga.java
@@ -2,7 +2,7 @@ package Tablicy;
 
 public class LiczbaArmstronga {
     public static void main(String[] args) {
-        int[] arr = {153, 333, 370, 515, 407, 80};
+        int[] arr = {153, 333, 370, 515, 407, 80};     // stworzylem tablice
         int potega = 0;
         double Arm = 0;
 
@@ -11,19 +11,19 @@ public class LiczbaArmstronga {
             int liczba = arr[i];
 
             while(liczba>0){
-                liczba = liczba/10;
+                liczba = liczba/10;    // licze ilosc znakow
                 potega++;
             }
             liczba = arr[i];
 
             while(liczba>0){
-                Arm = Arm + Math.pow(liczba%10, potega);
+                Arm = Arm + Math.pow(liczba%10, potega);     //szukam liczbe Armstronga
                 liczba = liczba/10;
             }
             potega = 0;
 
             if(Arm==arr[i]){
-                System.out.println("Jest liczba Armstronga: " + arr[i] + " pod indeksem: " + i);
+                System.out.println("Jest liczba Armstronga: " + arr[i] + " pod indeksem: " + i);      // Sprawdzam jest lub nie jest liczba Armstronga
             } else {
                 Arm = 0;
                 System.out.println("Nie jest liczba Armstronga: " + arr[i] + " pod indeksem: " + i);
