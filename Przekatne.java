7b74a076977fa186e122d7f6a8ad7768998cef20
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-11-25 23:06:29 +0100

    Update Przekatne.java

--- Przekatne.java
+++ Przekatne.java
@@ -15,10 +15,10 @@ public class Przekatne {
         float przekatnaPrawa = 0;
 
         for(int i = 0; i<tabl.length; i++){
-            przekatnaPrawa = przekatnaPrawa + tabl[i][i];
+            przekatnaPrawa = przekatnaPrawa + tabl[i][i];       // wlasciwosc przekatnej - indeksy sa rowne
         }
         for(int i = 0; i<tabl.length; i++){
-            przekatnaLewa = przekatnaLewa + tabl[i][tabl.length-i-1];
+            przekatnaLewa = przekatnaLewa + tabl[i][tabl.length-i-1];     // wlasciwosc anti-przekatnej - jeden od poczatku drugi od konca
         }
 
         System.out.println(przekatnaLewa);
